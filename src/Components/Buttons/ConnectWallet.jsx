6a2c8a1b721edab70fd91fb9fa5d4f490418e3a9
Chetan-B-B <chetanchetu9740@gmial.com>
2022-02-03 01:52:25 +0530

    completed

--- src/Components/Buttons/ConnectWallet.jsx
+++ src/Components/Buttons/ConnectWallet.jsx
@@ -1,10 +1,9 @@
-import React, { FC, useContext, useMemo } from "react";
+import React, { useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { WalletContext } from "@solana/wallet-adapter-react";
 import UserWallet from "../UserWallet/UserWallet";
 import classes from "./ConnectWallet.module.css";
 import {
@@ -27,16 +26,11 @@ import { clusterApiUrl } from "@solana/web3.js";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const ConnectWallet = () => {
-  const context = useContext(WalletContext);
-  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
   const network = WalletAdapterNetwork.Devnet;
 
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-  // Only the wallets you configure here will be compiled into your application, and only the dependencies
-  // of wallets that your users connect to will be loaded.
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
@@ -49,7 +43,6 @@ const ConnectWallet = () => {
     ],
     [network]
   );
-  console.log(context);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
